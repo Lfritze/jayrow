fdcc1917702b6fca630687857e0c088118ed3621
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-09-04 18:06:10 -0400

    updated server

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,10 @@
-// import express from 'express';
-// import path from 'path';
-// import mongoose from 'mongoose';
-// import bodyParser from 'body-parser';
-// import config from './config';
-// import userRoute from './routes/userRoute';
-// import productRoute from './routes/productRoute';
-// import orderRoute from './routes/orderRoute';
+import express from 'express';
+import data from './data';
 
-// import dotenv from 'dotenv';
+const app = express();
 
-// dotenv.config();
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
 
-// const mongodbUrl = config.MONGODB_URL;
-// mongoose.connect(mongodbUrl, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//   })
-//   .catch((error) => console.log(error.reason));
-
-// const app = express();
-// app.use(bodyParser.json());
-// app.use('/api/users', userRoute);
-// app.use('/api/products', productRoute);
-// app.use('/api/orders', orderRoute);
-// app.get('/api/config/paypal', (req, res) => {
-//   res.send(config.PAYPAL_CLIENT_ID);
-// });
-
-// // app.use(express.static(path.join(__dirname, '/../frontend/build')));
-// // app.get('*', (req, res) => {
-// //   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
-// // });
-
-// app.listen(config.PORT, () => {
-//   console.log('Server started at http://localhost:5000');
-// });
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
