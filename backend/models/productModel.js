dc8318c92079fe910f39192cc0fb5ca4e03fcd4c
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-09-04 20:10:13 -0400

    migrated screens on frontend

---
+++ backend/models/productModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+/* Product Schema */
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true},
+  image: { type: String, required: true},
+  brand: { type: String, required: true},
+  price: { type: Number, default: 0, required: true},
+  category: { type: String, required: true},
+  countInStock: { type: Number, default: 0, required: true},
+  description: { type: String, required: true},
+  rating: { type: Number, default: 0, required: true},
+  numReviews: { type: Number, default: 0, required: true}
+})
+
+/* Model */
+
+const productModel = mongoose.model("Product", productSchema);
+
+export default productModel;
