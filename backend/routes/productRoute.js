e21fc8680271c0f353ea297823784f7c513c7ab5
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-09-06 18:38:04 -0400

    repaired api/productid/

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -26,6 +26,15 @@ router.get('/', async (req, res) => {
   res.send(products);
 });
 
+router.get('/:id', async (req, res) => {
+  const product = await Product.findOne({ _id: req.params.id });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found.' });
+  }
+});
+
 router.post('/', isAuth, isAdmin, async(req, res) => {
   const product = new Product({
     name: req.body.name,
