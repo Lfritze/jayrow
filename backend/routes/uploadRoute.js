e570c98ed87f7f1a1bb574c24e0e9ddadee639cb
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-09-20 22:45:19 -0400

    finished setting up aws and image upload

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -2,15 +2,14 @@ import express from 'express';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import aws from 'aws-sdk';
-import { config } from 'dotenv/types';
-
+import config from '../config';
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, 'uploads/');
   },
   filename(req, file, cb) {
-    cb(null, `${Date.now()}.JPG`)
+    cb(null, `${Date.now()}.jpg`);
   },
 });
 
@@ -24,24 +23,20 @@ router.post('/', upload.single('image'), (req, res) => {
 
 aws.config.update({
   accessKeyId: config.accessKeyId,
-  secretAccessKey: config.secretAccessKey
+  secretAccessKey: config.secretAccessKey,
 });
-
 const s3 = new aws.S3();
-
 const storageS3 = multerS3({
   s3,
-  bucket: 'fritze-cards-bucket',
+  bucket: 'amazona-bucket',
   acl: 'public-read',
   contentType: multerS3.AUTO_CONTENT_TYPE,
   key(req, file, cb) {
-    cb(bull, file.originalname);
-  }
+    cb(null, file.originalname);
+  },
 });
-
 const uploadS3 = multer({ storage: storageS3 });
 router.post('/s3', uploadS3.single('image'), (req, res) => {
   res.send(req.file.location);
 });
-
 export default router;
